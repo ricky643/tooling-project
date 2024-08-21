8687ba97a63c27d6f5ad4855eb46601a13dc0100
ricky643 <143198084+ricky643@users.noreply.github.com>
2024-08-21 12:19:03 -0400

    Updated to include api token
    Updated to include api token

--- webserver.py
+++ webserver.py
@@ -2,12 +2,19 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
+
+
+#Include database here: 
+
 # Route for the homepage
 #route(/) decorator applied to the hello() function, meaning that the function will handle requests to the root URL ’/’
 @app.route("/", methods=["GET"])
 def hello():
     #test text to see that html server is online
     return "Hello Ricardo"
+
+def setToken(username, password):
 
 # Route to process user JSON data
 @app.route("/processUserJson", methods=["POST"])
@@ -16,9 +23,10 @@ def userJson():
     data = request.get_json()
     
     #retrieve post data
-    username = data.form.get('username')
-    password = data.form.get('password')
+    username = data.get('username')
+    password = data.get('password')
 
+    apiCode = 'unsure'
     #declare
     #api code    
     #dashboard code
@@ -26,11 +34,13 @@ def userJson():
     
     return jsonify({'result': 'Success', 'username': username, 'password': password})
 
+
+
     #user in database in toke
     #or create token when they login each time
 
 
 if __name__ == '__main__':
     # Looks for http traffic on port 8000 via running  the flask app
-    PORT = 8000 
+    PORT = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
